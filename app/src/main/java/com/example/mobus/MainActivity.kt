768d096e301a82b7380617a608442731305abcc1
Siegrfield <92269138+Siegrfield@users.noreply.github.com>
2022-09-17 12:24:12 -0300

    Primeiro commit branch front

--- app/src/main/java/com/example/mobus/MainActivity.kt
+++ app/src/main/java/com/example/mobus/MainActivity.kt
@@ -10,7 +10,7 @@ open class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
 
         val map : MapActivity = MapActivity()
